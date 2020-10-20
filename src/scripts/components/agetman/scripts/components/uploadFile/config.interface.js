b0511ebbaa50574735a8c607fb388c9a495636b2
Антон Круглый <A.Krougly@besmart.by>
2020-10-20 12:56:21 +0300

    delete all src

--- src/scripts/components/agetman/scripts/components/uploadFile/config.interface.js
+++
@@ -1,7 +0,0 @@
-export const ImportConfig = {
-  logging: false,
-  disableImportNew: false,
-  disableImportOverwrite: false,
-  preCommitCallback: null,
-  postCommitCallback: null,
-};
--- src/scripts/components/agetman/scripts/components/uploadFile/i18n/index.js
+++
@@ -1,3 +0,0 @@
-export { default as es } from './es';
-export { default as en } from './en';
-export { default as cn } from './cn';
